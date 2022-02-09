7a350225a2f8bdd8c42c8c7b43199607434121e6
ryhswang <xuehaq@gmail.com>
2022-02-09 20:10:50 +0800

    测试  EasyNetQ 的 Send/Receive 模式

--- QueueAndBus/LearnEasyNetQ/Subscriber/Program.cs
+++ QueueAndBus/LearnEasyNetQ/Subscriber/Program.cs
@@ -5,11 +5,37 @@ using TextMessageLib;
 //快速开始之订阅消息
 using (var bus = RabbitHutch.CreateBus("host=localhost"))
 {
-    bus.PubSub.Subscribe<TextMessage>("test", HandleTextMessage);
     Console.WriteLine("Listening for messages. Hit <return> to quit.");
+
+    //using Subscribers
+    bus.PubSub.Subscribe<TextMessage>("test", message =>
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Got message: {0}", message.Text);
+        Console.ResetColor();
+    });
+
+
+
+    //------------>我是分割线---------------->
+
+
+
+
+    //using Send
+    ISendReceive Ireceive = bus.SendReceive;
+    Ireceive.Receive<TextMessage>("my.queue", message =>
+    {
+        //HandleTextMessage(message);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Got message: {0}", message.Text);
+        Console.ResetColor();
+    });
     Console.ReadLine();
 }
+
 Console.ReadKey();
+
 
 static void HandleTextMessage(TextMessage textMessage)
 {
