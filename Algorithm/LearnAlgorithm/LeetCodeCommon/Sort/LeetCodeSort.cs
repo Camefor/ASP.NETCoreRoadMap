18335e25afcfe5c92a1f6479614aa5b8883814b5
rhys wang <xuehaq@gmail.com>
2022-05-10 21:24:42 +0800

    add MergeSort

--- Algorithm/LearnAlgorithm/LeetCodeCommon/Sort/LeetCodeSort.cs
+++ Algorithm/LearnAlgorithm/LeetCodeCommon/Sort/LeetCodeSort.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
+//https://www.youtube.com/watch?v=UOa3dQT3hOc&list=PLFOgdf0iZ6uskTJam3P2BZwpdAGGG0e24&index=4
 namespace LearnAlgorithm.LeetCodeCommon.Sort
 {
+
+
     /// <summary>
-    /// 排序算法
+    /// 排序算法:
     /// </summary>
     public class LeetCodeSort
     {
@@ -39,11 +42,84 @@ namespace LearnAlgorithm.LeetCodeCommon.Sort
             return nums;
         }
 
+        /// <summary>
+        /// 插入排序算法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] InsertSort(int[] nums)
+        {
+            int current = 0;
+            for (int i = 1; i < nums.Length; i++) //int 默认等于1  默认数组的第一个元素已经排好序
+            {
+                int j = i;
+                current = nums[i]; //  以便将来把它插入到合适的位置
+                //内循环
+                for (j = i - 1; j >= 0 && nums[j] > current; j--)
+                {
+                    nums[j + 1] = nums[j]; //如果当前j所指向的值比current大  就把该数往右移动一位
+                }
+                //内循环结束后 j + 1 所指向的位置  就是要把current 插入的位置
+                nums[j + 1] = current;
+            }
 
-        //public int[] InsertSort(int[] nums)
-        //{
+            return nums;
+        }
 
-        //}
+
+        /// <summary>
+        /// 归并排序算法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] MergeSort(int[] A)
+        {
+            int lo = 0;
+            int hi = A.Length - 1;
+
+            int[] result = new int[hi];
+            MergeSortHandler(A, lo, hi, ref result);
+            return result;
+        }
+        void MergeSortHandler(int[] A, int lo, int hi, ref int[] result)
+        {
+            if (lo >= hi) return; //只剩下最后一个元素
+
+            int mid = lo + (hi - lo) / 2; //分两半
+            MergeSortHandler(A, lo, mid, ref result);
+            MergeSortHandler(A, mid + 1, hi, ref result);
+
+            result = merage(A, lo, mid, hi);
+        }
+
+        int[] merage(int[] nums, int lo, int mid, int hi)
+        {
+            int[] copy = nums.Clone() as int[];
+
+            int k = lo, i = lo, j = mid + 1;
+
+            while (k <= hi)
+            {
+                if (i > mid)
+                {
+                    nums[k++] = copy[j++];
+                }
+                else if (j > hi)
+                {
+                    nums[k++] = copy[i++];
+                }
+                else if (copy[j] < copy[i])
+                {
+                    nums[k++] = copy[j++];
+                }
+                else
+                {
+                    nums[k++] = copy[i++];
+                }
+            }
+
+            return nums;
+        }
 
     }
 }
