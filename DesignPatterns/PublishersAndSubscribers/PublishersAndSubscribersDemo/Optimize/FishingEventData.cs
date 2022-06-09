9d22a263794e421fa8870b4bb7cb4a485d20dc9f
rhys wang <xuehaq@gmail.com>
2022-06-09 16:54:27 +0800

    添加 设计模式 学习模块:（发布-订阅）模式
    更新 在初步实现后 抽象事件源和事件处理

---
+++ DesignPatterns/PublishersAndSubscribers/PublishersAndSubscribersDemo/Optimize/FishingEventData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublishersAndSubscribersDemo.Optimize
+{
+
+    /// <summary>
+    ///   “钓鱼” 这个事件
+    /// </summary>
+    public class FishingEventData : EventData
+    {
+        public FishTypeEnum FishType { get; set; }
+
+        public FishingMan FisingMan { get; set; }
+        
+    }
+}
