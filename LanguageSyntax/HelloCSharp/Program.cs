22a19e745c0b7094786778f7adb0d58e70181601
ryhswang <xuehaq@gmail.com>
2022-06-23 13:27:09 +0800

    update

--- LanguageSyntax/HelloCSharp/Program.cs
+++ LanguageSyntax/HelloCSharp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var result = TestTryCatch();
+
+
+                Console.WriteLine();
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.WriteLine("运行结束……");
+            Console.ReadKey();
+        }
+
+        static string TestTryCatch()
+        {
+            string res_msg = string.Empty;
+            try
+            {
+                if (new Random().Next(1, 2) != 2)
+                {
+                    throw new Exception("模拟异常");
+                }
+                res_msg = "我是返回的数据";
+                return "我是返回的数据";
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("finally \r\n");
+                Console.WriteLine("finally after " + res_msg);
+            }
+
+        }
+    }
+}
