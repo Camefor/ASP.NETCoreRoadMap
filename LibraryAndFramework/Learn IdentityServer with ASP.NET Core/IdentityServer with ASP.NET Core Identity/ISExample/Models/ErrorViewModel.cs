50b574ad1fd2eb217c075c1dee701b4f146d1843
rhys wang <xuehaq@gmail.com>
2022-07-01 10:38:37 +0800

    update ISExample Project

---
+++ LibraryAndFramework/Learn IdentityServer with ASP.NET Core/IdentityServer with ASP.NET Core Identity/ISExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LibraryAndFramework/Learn IdentityServer with ASP.NET Core/IdentityServer with ASP.NET Core Identity/ISExample/Settings/IdentityServerSettings.cs
@@ -0,0 +1,19 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace ISExample.Settings
+{
+    public class IdentityServerSettings
+    {
+
+        public IReadOnlyCollection<ApiScope> ApiScopes { get; set; }
+        public IReadOnlyCollection<ApiResource> ApiResources { get; set; }
+        public IReadOnlyCollection<Client> Clients { get; set; }
+        public IReadOnlyCollection<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    }
+}
