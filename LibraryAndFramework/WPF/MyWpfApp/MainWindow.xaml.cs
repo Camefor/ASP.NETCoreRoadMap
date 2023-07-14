d91f64fe56fb088ce2966ed1a1630041c0fc2aab
ryhswang <xuehaq@gmail.com>
2023-07-14 17:04:51 +0800

    Freezable

---
+++ LibraryAndFramework/WPF/MyWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWpfApp
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private People aMan;
+        public MainWindow()
+        {
+            aMan = new People();
+            aMan.Name = "Rhys";
+
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (aMan.CanFreeze)
+            {
+                aMan.Freeze();
+                MessageBox.Show("冻结对象成功");
+            }
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                aMan.Name = "Tim";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
