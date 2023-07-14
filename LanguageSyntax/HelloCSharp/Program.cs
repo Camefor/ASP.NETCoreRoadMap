93a39fd4e1d84cd37517acdbf38e438becf6d498
rhys wang <xuehaq@gmail.com>
2023-07-14 17:04:51 +0800

    Freezable

--- LanguageSyntax/HelloCSharp/Program.cs
+++ LanguageSyntax/HelloCSharp/Program.cs
@@ -31,53 +31,10 @@
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var result = TestTryCatch();
-
-
-                Console.WriteLine();
-                Console.WriteLine(result);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-            }
+            
             Console.WriteLine("运行结束……");
             Console.ReadKey();
         }
 
-        static string TestTryCatch()
-        {
-            string res_msg = string.Empty;
-            try
-            {
-                if (new Random().Next(1, 2) != 2)
-                {
-                    res_msg = "模拟异常 即将抛出异常";
-
-                    throw new MyException("模拟异常");
-                }
-                res_msg = "我是返回的数据";
-                return "我是返回的数据";
-            }
-            catch (MyException mex)
-            {
-                throw;
-
-            }
-            catch (Exception ex)
-            {
-
-                throw;
-            }
-            finally
-            {
-                Console.WriteLine("finally \r\n");
-                Console.WriteLine("finally after " + res_msg);
-            }
-
-        }
     }
 }
