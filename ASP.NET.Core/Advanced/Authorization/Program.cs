441a97d4bd3a22a296aedf666d1c5427503553f6
rusheng.wang <xuehaq@gmail.com>
2025-05-21 16:21:50 +0800

    🍓 How does the app.UseAuthorization() method work?

--- ASP.NET.Core/Advanced/Authorization/Program.cs
+++ ASP.NET.Core/Advanced/Authorization/Program.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 const string AuthScheme = "cookie";
 const string AuthScheme2 = "cookie2";
@@ -7,8 +8,17 @@ const string AuthScheme2 = "cookie2";
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(AuthScheme)
     .AddCookie(AuthScheme)
-    .AddCookie(AuthScheme2);
-    //注册认证服务
+    .AddCookie(AuthScheme2); //注册认证服务
+
+builder.Services.AddAuthorization(builder =>
+{
+    builder.AddPolicy("eu passport", pb =>
+    {
+        pb.RequireAuthenticatedUser()
+            .AddAuthenticationSchemes(AuthScheme)
+            .RequireClaim("passport_type", "eur");
+    });
+}); //注册授权服务
 
 var app = builder.Build();
 
@@ -17,37 +27,34 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
-app.Use((ctx, next) =>
-{
-    if (ctx.Request.Path.StartsWithSegments("/login"))
-    {
-        return next();
-    }
-    
-    if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
-    {
-        ctx.Response.StatusCode = 401;
-        // return "Unauthenticated"; //未认证
-        //这一步仅检查是否登陆 有这个用户 不进行权限校验 只进行登陆校验
-        return Task.CompletedTask;
-    }
-    
-    if (!ctx.User.HasClaim("passport_type", "eur") && !ctx.User.HasClaim("passport_type", "NOR"))
-    {
-        ctx.Response.StatusCode = 403;
-        // return "Unauthorized"; //未授权
-        return Task.CompletedTask;
-    }
-    
-    return next();
-});
+app.UseAuthorization();
+// app.Use((ctx, next) =>
+// {
+//     if (ctx.Request.Path.StartsWithSegments("/login"))
+//     {
+//         return next();
+//     }
+//     
+//     if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+//     {
+//         ctx.Response.StatusCode = 401;
+//         // return "Unauthenticated"; //未认证
+//         //这一步仅检查是否登陆 有这个用户 不进行权限校验 只进行登陆校验
+//         return Task.CompletedTask;
+//     }
+//     
+//     if (!ctx.User.HasClaim("passport_type", "eur") && !ctx.User.HasClaim("passport_type", "NOR"))
+//     {
+//         ctx.Response.StatusCode = 403;
+//         // return "Unauthorized"; //未授权
+//         return Task.CompletedTask;
+//     }
+//     
+//     return next();
+// });
 
-
-app.MapGet("unsecure", (HttpContext ctx) =>
-{
-    
-    return ctx.User.FindFirst("usr")?.Value ?? "empty";
-});
+// [Authorize(Policy = "eu passport")] //when its Controller API endpoint
+app.MapGet("unsecure", (HttpContext ctx) => { return ctx.User.FindFirst("usr")?.Value ?? "empty"; }).RequireAuthorization("eu passport");
 
 
 app.MapGet("/norway", (HttpContext ctx) =>
@@ -59,7 +66,7 @@ app.MapGet("/norway", (HttpContext ctx) =>
     //     return "Unauthenticated"; //未认证
     //     //这一步仅检查是否登陆 有这个用户 不进行权限校验 只进行登陆校验
     // }
-    
+
     if (!ctx.User.HasClaim("passport_type", "NOR"))
     {
         ctx.Response.StatusCode = 403;
@@ -67,7 +74,7 @@ app.MapGet("/norway", (HttpContext ctx) =>
     }
 
     return "allowed";
-});
+}).RequireAuthorization("eu passport");
 
 // [AuthScheme(AuthScheme)]
 // [AuthClaim]
@@ -91,6 +98,6 @@ app.MapGet("/login", async (HttpContext ctx) =>
     var identity = new ClaimsIdentity(claims, AuthScheme);
     var user = new ClaimsPrincipal(identity);
     await ctx.SignInAsync(AuthScheme, user);
-});
+}).AllowAnonymous();
 
 app.Run();
